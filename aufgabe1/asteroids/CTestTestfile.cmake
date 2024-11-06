1437737b039a426de64c7610dc0237092fe81bae
ShariKahl <shari.kahl@gmx.de>
2024-11-06 00:33:37 +0000

    03_asteroids added

---
+++ aufgabe1/asteroids/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kash1012/Documents/Computergrafik/aufgabe1/asteroids
+# Build directory: /home/kash1012/Documents/Computergrafik/aufgabe1/asteroids
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
