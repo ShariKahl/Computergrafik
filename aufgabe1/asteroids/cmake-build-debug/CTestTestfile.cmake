78210aeca0ce18a6cccd3f902e7b4707d3ef3fc8
shari <shari.kahl@gmx.de>
2024-12-13 11:40:48 +0000

    added source code to aufgabe2

--- aufgabe1/asteroids/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kash1012/Documents/Computergrafik/aufgabe1/asteroids
-# Build directory: /home/kash1012/Documents/Computergrafik/aufgabe1/asteroids/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
