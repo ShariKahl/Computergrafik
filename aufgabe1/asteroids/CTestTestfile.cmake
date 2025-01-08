54545b11e0c87f1d4e261c71424c7d4540c745b1
ShariKahl <shari.kahl@gmx.de>
2025-01-08 17:28:59 +0100

    6. Ubungsblatt Vorbereitungen

--- aufgabe1/asteroids/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shari/CLionProjects/Computergrafik/aufgabe1/asteroids
-# Build directory: /home/shari/CLionProjects/Computergrafik/aufgabe1/asteroids
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
